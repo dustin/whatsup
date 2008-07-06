c0b798c1b23159984b59c9d881f7cc741bd244ea
Dustin Sallings <dustin@spy.net>
2008-07-06 00:29:44 -0700

    Be sure to flush the thread error dumping.

--- whatsup/threading.rb
+++ whatsup/threading.rb
@@ -13,8 +13,10 @@ module Whatsup
             msg.call
           rescue StandardError, Interrupt
             puts "ERROR!  #{$!}\n#{$!.backtrace.join("\n\t")}"
+            $stdout.flush
           rescue
             puts "ERROR!  #{$!}\n#{$!.backtrace.join("\n\t")}"
+            $stdout.flush
           end
         end
       end
