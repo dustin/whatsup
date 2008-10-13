44eedfd425d9ffa15fd36c2e2e6b80200b5cc5d8
Dustin Sallings <dustin@spy.net>
2008-10-13 09:32:32 -0700

    Subprocess for version.

--- lib/wu_config.py
+++ lib/wu_config.py
@@ -6,7 +6,9 @@ Copyright (c) 2008  Dustin Sallings <dustin@spy.net>
 """
 
 import ConfigParser
+import commands
 
 CONF=ConfigParser.ConfigParser()
 CONF.read('whatsup.conf')
 SCREEN_NAME = CONF.get('xmpp', 'jid')
+VERSION=commands.getoutput("git describe").strip()
