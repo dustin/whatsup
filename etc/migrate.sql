b70bd545ccb7c2b09751218472cf609d1b96cf1c
Dustin Sallings <dustin@spy.net>
2008-10-13 01:16:10 -0700

    Also rewrite the patterns.

--- etc/migrate.sql
+++ etc/migrate.sql
@@ -2,6 +2,8 @@ update users set active=1 where active='t';
 update users set active=0 where active='f';
 update watches set active=0 where active='f';
 update watches set active=1 where active='t';
+update patterns set positive=1 where positive='t';
+update patterns set positive=0 where positive='f';
 
 update watches set last_update = replace(substr(last_update, 1, 19), 'T', ' ');
 update watches set quiet_until = null;
