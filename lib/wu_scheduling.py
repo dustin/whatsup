68988228c071030c9c410b38745c840e7f67032f
Dustin Sallings <dustin@spy.net>
2008-10-12 23:55:04 -0700

    Proper reporting from background jobs.

--- lib/wu_scheduling.py
+++ lib/wu_scheduling.py
@@ -1,5 +1,6 @@
 import models
 
+import datetime
 from twisted.web import client
 
 class CheckSites(object):
@@ -19,8 +20,29 @@ class CheckSites(object):
     def __mkCallback(self, id, f):
         return lambda v: f(id, v)
 
+    def __updateDb(self, watch, status, session):
+        watch.status=status
+        watch.last_update = datetime.datetime.now()
+        session.commit()
+
     def onSuccess(self, watch_id, page):
         print "Success fetching %d: %d bytes" % (watch_id, len(page))
+        session = models.Session()
+        watch=session.query(models.Watch).filter_by(id=watch_id).one()
+        if 200 != watch.status:
+            self.client.send_plain(watch.user.jid,
+                ":) Status of %s changed from %s to %d"
+                % (watch.url, `watch.status`, 200))
+        self.__updateDb(watch, 200, session)
 
     def onError(self, watch_id, error):
         print "Error fetching %d: %s" % (watch_id, error)
+        session = models.Session()
+        watch=session.query(models.Watch).filter_by(id=watch_id).one()
+        try:
+            status=int(error.getErrorMessage()[0:3])
+        except:
+            status=-1
+        self.client.send_plain(watch.user.jid, ":( Error in %s: %d - %s"
+            % (watch.url, status, error.getErrorMessage()))
+        self.__updateDb(watch, status, session)
